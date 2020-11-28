a8b13e9477c8c5be032551e837e0dba05b0c30d2
Tyler Pooschke <tjpoo92@gmail.com>
2020-11-28 13:12:56 -0600

    formatted page for media and large screens, logic in place for clear button

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 
 const container = document.querySelector(".nested-container")
 
-for (let i = 0; i < 16; i++) {
+
+let gridSizeSquared = 16;
+let gridSize = 4;
+let columns = `--columns: ${gridSize}`;
+
+container.style = columns;
+for (let i = 0; i < gridSizeSquared; i++) {
     const div = document.createElement("div")
-
     container.appendChild(div);
-
-    div.innerHTML = `<p>${i}</p>`
 }
-container.style = "--columns: 4";
+
+
+const clearButton = document.querySelector("button").addEventListener("click", userPrompt)
+
+
+function userPrompt() {
+    gridSize = prompt("Choose a number between 1 - 100", "4")
+    gridSize = parseInt(gridSize)
+    if ((gridSize >= 1) && (gridSize <= 100)) {
+        gridSizeSquared = gridSize * gridSize;
+        columns = `--columns: ${gridSize}`
+        while (container.hasChildNodes()) {
+            container.removeChild(container.firstChild);
+        }
+        generateGrid(gridSize, gridSizeSquared, columns)
+    }
+}
+function generateGrid(gridSize, gridSizeSquared, columns) {
+
+    for (let i = 0; i < gridSizeSquared; i++) {
+        const div = document.createElement("div")
+        container.appendChild(div);
+    }
+    container.style = columns;
+}
 
 // mouseover > color change per default and customer selection
 
