f397bd0c7c52407a588db89b7cd08d2f0e6028ee
Tyler Pooschke <tjpoo92@gmail.com>
2020-11-28 21:13:44 -0600

    logic for radio selection, and color change, refactoring

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 
 const container = document.querySelector(".nested-container")
-
-
 let gridSizeSquared = 16;
 let gridSize = 4;
 let columns = `--columns: ${gridSize}`;
-
 container.style = columns;
-for (let i = 0; i < gridSizeSquared; i++) {
-    const div = document.createElement("div")
-    container.appendChild(div);
+window.onload = generateGrid(gridSize, gridSizeSquared, columns)
+
+let userChoice
+const radio = document.querySelectorAll("input")
+for (let i = 0; i < radio.length; i++) {
+    radio[i].addEventListener("click", (e) => {
+        userChoice = e.target.value;
+        return userChoice;
+    })
 }
 
-
-const clearButton = document.querySelector("button").addEventListener("click", userPrompt)
-
-
+document.querySelector("button").addEventListener("click", userPrompt)
 function userPrompt() {
     gridSize = prompt("Choose a number between 1 - 100", "4")
     gridSize = parseInt(gridSize)
@@ -29,10 +29,14 @@ function userPrompt() {
     }
 }
 function generateGrid(gridSize, gridSizeSquared, columns) {
-
     for (let i = 0; i < gridSizeSquared; i++) {
         const div = document.createElement("div")
         container.appendChild(div);
+        div.addEventListener("mouseenter", () => {
+            if (userChoice == "random") { div.style.backgroundColor = "blue" }
+            else if (userChoice == "fade-to-black") { div.style.backgroundColor = "black" }
+            else { div.style.backgroundColor = "red" }
+        })
     }
     container.style = columns;
 }
